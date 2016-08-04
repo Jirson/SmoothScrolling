956755800bc7b8c80c811448f1a3166082549505
Jirson Tavera <JirsonTavera@hotmail.com>
2016-08-04 11:41:07 -0500

    smoothScrolling

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="es-CO">
+    <head>
+        <meta charset="utf-8">
+        <title>SmoothScrolling</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <style>
+            section:first-of-type {
+                margin-top: 80px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false"></button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="main-nav">
+                        <ul class="nav navbar-nav">
+                            <li><a class="link" href="#section1">Sección 1</a></li>
+                            <li><a class="link" href="#section2">Sección 2</a></li>
+                            <li><a class="link" href="#section3">Sección 3</a></li>
+                            <li><a class="link" href="#section4">Sección 4</a></li>
+                        </ul>
+                    </div>
+                </div> <!-- /container-fluid -->
+                </nav>
+            </header>
+            <div class="main">
+                <section id="section1">
+                    <p>Lorem ipsum dolor sit amet, eum in natum nominati, id sit perpetua rationibus. Case fugit tation eam id, eam sint nonumy aliquip in. Vel aeque cetero et, pro ea dico oportere definiebas. Integre accumsan ut mei, ea ludus malorum menandri vis. An his affert delenit disputando, ad eam utamur oportere indoctum.
+
+Id vis ludus cetero. Prompta gloriatur ullamcorper pro in, an iudicabit sententiae eos, vel alienum accusata conceptam in. Modo essent delectus nam ei, eos oratio homero an. In commune expetenda mediocritatem usu, an sea graece discere epicuri.
+
+Aperiri repudiandae nec ne, an vim magna detracto constituam. Iisque nostrud assueverit in est, cibo quaeque vituperata vix ex, cu per ancillae placerat platonem. Est in aliquid inermis, aperiri dolores eam ea. Cum munere ancillae ullamcorper te. Elaboraret consequuntur eam ne, qui id natum fugit regione. No vix illud essent. Denique apeirian tacimates quo an, id sea zril eirmod, hendrerit temporibus eu mea.
+
+Suas vide ne has. His in adhuc euripidis, ad eam audire detracto, reque tamquam sanctus has et. Qui nihil zril aliquam at, eu odio oratio persius qui. Ex eam periculis incorrupte, laoreet corrumpit nec ad, ea sea ignota veritus ullamcorper. An solum iudico dissentias cum, quo case accusata atomorum et.
+
+Ex iudico labores vituperata vix, pro erant feugait incorrupte ea. Nam ea ferri assentior suscipiantur, docendi praesent per id. Libris accusamus no nam, sed eu integre senserit. Inermis delicata usu te, vel te augue voluptua invidunt, et dolore quodsi detracto ius. Cu ullum laudem pericula qui, atqui noster conclusionemque ne eam.</p>
+                </section>
+                <section id="section2">
+                    <p>Lorem ipsum dolor sit amet, eum in natum nominati, id sit perpetua rationibus. Case fugit tation eam id, eam sint nonumy aliquip in. Vel aeque cetero et, pro ea dico oportere definiebas. Integre accumsan ut mei, ea ludus malorum menandri vis. An his affert delenit disputando, ad eam utamur oportere indoctum.
+
+Id vis ludus cetero. Prompta gloriatur ullamcorper pro in, an iudicabit sententiae eos, vel alienum accusata conceptam in. Modo essent delectus nam ei, eos oratio homero an. In commune expetenda mediocritatem usu, an sea graece discere epicuri.
+
+Aperiri repudiandae nec ne, an vim magna detracto constituam. Iisque nostrud assueverit in est, cibo quaeque vituperata vix ex, cu per ancillae placerat platonem. Est in aliquid inermis, aperiri dolores eam ea. Cum munere ancillae ullamcorper te. Elaboraret consequuntur eam ne, qui id natum fugit regione. No vix illud essent. Denique apeirian tacimates quo an, id sea zril eirmod, hendrerit temporibus eu mea.
+
+Suas vide ne has. His in adhuc euripidis, ad eam audire detracto, reque tamquam sanctus has et. Qui nihil zril aliquam at, eu odio oratio persius qui. Ex eam periculis incorrupte, laoreet corrumpit nec ad, ea sea ignota veritus ullamcorper. An solum iudico dissentias cum, quo case accusata atomorum et.
+
+Ex iudico labores vituperata vix, pro erant feugait incorrupte ea. Nam ea ferri assentior suscipiantur, docendi praesent per id. Libris accusamus no nam, sed eu integre senserit. Inermis delicata usu te, vel te augue voluptua invidunt, et dolore quodsi detracto ius. Cu ullum laudem pericula qui, atqui noster conclusionemque ne eam.</p>
+                </section>
+                <section id="section3">
+                    <p>Lorem ipsum dolor sit amet, eum in natum nominati, id sit perpetua rationibus. Case fugit tation eam id, eam sint nonumy aliquip in. Vel aeque cetero et, pro ea dico oportere definiebas. Integre accumsan ut mei, ea ludus malorum menandri vis. An his affert delenit disputando, ad eam utamur oportere indoctum.
+
+Id vis ludus cetero. Prompta gloriatur ullamcorper pro in, an iudicabit sententiae eos, vel alienum accusata conceptam in. Modo essent delectus nam ei, eos oratio homero an. In commune expetenda mediocritatem usu, an sea graece discere epicuri.
+
+Aperiri repudiandae nec ne, an vim magna detracto constituam. Iisque nostrud assueverit in est, cibo quaeque vituperata vix ex, cu per ancillae placerat platonem. Est in aliquid inermis, aperiri dolores eam ea. Cum munere ancillae ullamcorper te. Elaboraret consequuntur eam ne, qui id natum fugit regione. No vix illud essent. Denique apeirian tacimates quo an, id sea zril eirmod, hendrerit temporibus eu mea.
+
+Suas vide ne has. His in adhuc euripidis, ad eam audire detracto, reque tamquam sanctus has et. Qui nihil zril aliquam at, eu odio oratio persius qui. Ex eam periculis incorrupte, laoreet corrumpit nec ad, ea sea ignota veritus ullamcorper. An solum iudico dissentias cum, quo case accusata atomorum et.
+
+Ex iudico labores vituperata vix, pro erant feugait incorrupte ea. Nam ea ferri assentior suscipiantur, docendi praesent per id. Libris accusamus no nam, sed eu integre senserit. Inermis delicata usu te, vel te augue voluptua invidunt, et dolore quodsi detracto ius. Cu ullum laudem pericula qui, atqui noster conclusionemque ne eam.</p>
+                </section>
+                <section id="section4">
+                    <p>Lorem ipsum dolor sit amet, eum in natum nominati, id sit perpetua rationibus. Case fugit tation eam id, eam sint nonumy aliquip in. Vel aeque cetero et, pro ea dico oportere definiebas. Integre accumsan ut mei, ea ludus malorum menandri vis. An his affert delenit disputando, ad eam utamur oportere indoctum.
+
+Id vis ludus cetero. Prompta gloriatur ullamcorper pro in, an iudicabit sententiae eos, vel alienum accusata conceptam in. Modo essent delectus nam ei, eos oratio homero an. In commune expetenda mediocritatem usu, an sea graece discere epicuri.
+
+Aperiri repudiandae nec ne, an vim magna detracto constituam. Iisque nostrud assueverit in est, cibo quaeque vituperata vix ex, cu per ancillae placerat platonem. Est in aliquid inermis, aperiri dolores eam ea. Cum munere ancillae ullamcorper te. Elaboraret consequuntur eam ne, qui id natum fugit regione. No vix illud essent. Denique apeirian tacimates quo an, id sea zril eirmod, hendrerit temporibus eu mea.
+
+Suas vide ne has. His in adhuc euripidis, ad eam audire detracto, reque tamquam sanctus has et. Qui nihil zril aliquam at, eu odio oratio persius qui. Ex eam periculis incorrupte, laoreet corrumpit nec ad, ea sea ignota veritus ullamcorper. An solum iudico dissentias cum, quo case accusata atomorum et.
+
+Ex iudico labores vituperata vix, pro erant feugait incorrupte ea. Nam ea ferri assentior suscipiantur, docendi praesent per id. Libris accusamus no nam, sed eu integre senserit. Inermis delicata usu te, vel te augue voluptua invidunt, et dolore quodsi detracto ius. Cu ullum laudem pericula qui, atqui noster conclusionemque ne eam.</p>
+                </section>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script>
+        $(function () {
+            console.info("test");
+            $('.link').on('click', function (e) {
+                var target = $(this.hash);
+                $('html,body').animate({
+                   scrollTop: target.offset().top,
+                }, 1000);
+                console.info($(this.hash));
+            })
+        })
+        </script>
+    </body>
+</html>
